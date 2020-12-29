e38892ec22d6e65475da5c66bdf29059984d99bf
SunEom <dkxl0701@gmail.com>
2020-12-29 14:11:27 +0900

    Error handling

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
 
   const onChange = (event) => {
     const {
@@ -27,7 +28,7 @@ const Auth = () => {
       }
       console.log(data);
     } catch (err) {
-      console.log(err);
+      setError(err.message);
     }
   };
   return (
@@ -36,6 +37,7 @@ const Auth = () => {
         <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
         <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
         <input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
+        {error}
       </form>
       <div>
         <button>Continue with Google</button>
