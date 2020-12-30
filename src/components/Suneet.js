9155f2a9d1028c2c9d1f3cae0664846bdd9af134
SunEom <dkxl0701@gmail.com>
2020-12-30 13:57:06 +0900

    Update to delete Suneet

--- src/components/Suneet.js
+++ src/components/Suneet.js
@@ -1,12 +1,20 @@
+import { dbService } from 'myBase';
 import React from 'react';
 
 const Suneet = ({ suneetObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm('Are you sure you want to delete this Suneet?');
+    console.log(ok);
+    if (ok) {
+      await dbService.doc(`suneets/${suneetObj.id}`).delete();
+    }
+  };
   return (
     <div>
       <h4>{suneetObj.text}</h4>
       {isOwner && (
         <>
-          <button>Delete Suneet</button>
+          <button onClick={onDeleteClick}>Delete Suneet</button>
           <button>Edit Suneet</button>
         </>
       )}
