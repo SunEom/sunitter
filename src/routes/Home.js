d342b57017c90cb60561177bf6a1ba84f8d40c04
SunEom <dkxl0701@gmail.com>
2020-12-30 22:48:18 +0900

    Cleaning code

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,10 @@
 import Suneet from 'components/Suneet';
-import { dbService, storageService } from 'myBase';
+import SuneetFactory from 'components/SuneetFactory';
+import { dbService } from 'myBase';
 import React, { useEffect, useState } from 'react';
-import { v4 as uudiv4 } from 'uuid';
 
 const Home = ({ userObj }) => {
-  const [suneet, setSuneet] = useState('');
   const [suneets, setSuneets] = useState([]);
-  const [attachment, setAttachment] = useState('');
 
   useEffect(() => {
     dbService.collection('suneets').onSnapshot((snapshot) => {
@@ -15,62 +13,9 @@ const Home = ({ userObj }) => {
     });
   }, []);
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    let attachmentUrl = '';
-    if (attachment !== '') {
-      const attachmentRef = storageService.ref().child(`${userObj.uid}/${uudiv4()}`);
-      const response = await attachmentRef.putString(attachment, 'data_url');
-      attachmentUrl = await response.ref.getDownloadURL();
-    }
-    const suneetObj = {
-      text: suneet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-      attachmentUrl,
-    };
-
-    await dbService.collection('suneets').add(suneetObj);
-    setSuneet('');
-    setAttachment(null);
-  };
-
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setSuneet(value);
-  };
-
-  const onFileChange = (event) => {
-    const {
-      target: { files },
-    } = event;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-
-  const onClearAttachmentClick = () => setAttachment(null);
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={suneet} type="text" required placeholder="What's on your mind?" maxLength={120} />
-        <input type="file" accept="image/*" onChange={onFileChange} />
-        <input type="submit" value="Suneet" />
-        {attachment && (
-          <div>
-            <img src={attachment} alt="#" width="50px" height="50px" />
-            <button onClick={onClearAttachmentClick}>Clear</button>
-          </div>
-        )}
-      </form>
+      <SuneetFactory userObj={userObj} />
       <div>
         {suneets.map((suneet) => (
           <Suneet key={suneet.id} suneetObj={suneet} isOwner={suneet.creatorId === userObj.uid} />
