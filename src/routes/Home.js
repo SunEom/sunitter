6bb8fb40def18a9d8b8ac8804a12838751ccc8c9
SunEom <dkxl0701@gmail.com>
2020-12-30 14:30:14 +0900

    Update filereader

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -26,10 +26,23 @@ const Home = ({ userObj }) => {
     } = event;
     setSuneet(value);
   };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      console.log(finishedEvent);
+    };
+    reader.readAsDataURL(theFile);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={suneet} type="text" required placeholder="What's on your mind?" maxLength={120} />
+        <input type="file" accept="image/*" onChange={onFileChange} />
+
         <input type="submit" value="Suneet" />
       </form>
       <div>
