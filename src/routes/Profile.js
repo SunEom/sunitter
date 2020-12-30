21cf1715cc2304a03292917655592359568c7b7d
SunEom <dkxl0701@gmail.com>
2020-12-30 22:35:39 +0900

    Update username in realtime

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,22 +1,46 @@
 import { authService, dbService } from 'myBase';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-const Profile = ({ userObj }) => {
+const Profile = ({ userObj, refreshUser }) => {
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const history = useHistory();
+
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
   };
   const getMySuneets = async () => {
     const suneets = await dbService.collection('suneets').where('creatorId', '==', userObj.uid).orderBy('createdAt').get();
-    console.log(suneets.docs.map((doc) => doc.data()));
   };
+
   useEffect(() => {
     getMySuneets();
   }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (newDisplayName !== userObj.displayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} type="text" placeholder="Display name" value={newDisplayName} />
+        <input type="submit" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
