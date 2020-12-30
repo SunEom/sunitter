b4cf3f71be83c3aafbcbc2958b22a93f2ea5e3ab
SunEom <dkxl0701@gmail.com>
2020-12-30 12:55:42 +0900

    Create suneet form

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [suneet, setSuneet] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setSuneet(value);
+  };
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="submit" value="Suneet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
