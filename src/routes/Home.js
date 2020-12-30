fbbe27f72c1004ce2529266c4010398c688d6187
SunEom <dkxl0701@gmail.com>
2020-12-30 14:37:40 +0900

    Uploaded photo preview

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 const Home = ({ userObj }) => {
   const [suneet, setSuneet] = useState('');
   const [suneets, setSuneets] = useState([]);
+  const [attachment, setAttachment] = useState();
   useEffect(() => {
     dbService.collection('suneets').onSnapshot((snapshot) => {
       const suneetArray = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
@@ -33,17 +34,26 @@ const Home = ({ userObj }) => {
     const theFile = files[0];
     const reader = new FileReader();
     reader.onloadend = (finishedEvent) => {
-      console.log(finishedEvent);
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
     };
     reader.readAsDataURL(theFile);
   };
+  const onClearAttachmentClick = () => setAttachment(null);
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={suneet} type="text" required placeholder="What's on your mind?" maxLength={120} />
         <input type="file" accept="image/*" onChange={onFileChange} />
-
         <input type="submit" value="Suneet" />
+        {attachment && (
+          <div>
+            <img src={attachment} alt="#" width="50px" height="50px" />
+            <button onClick={onClearAttachmentClick}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {suneets.map((suneet) => (
