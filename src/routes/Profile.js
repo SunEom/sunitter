0080942004114f632b84ecfc9184d47c41e37831
SunEom <dkxl0701@gmail.com>
2020-12-30 22:06:36 +0900

    Filtering suneet by creatorId

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,20 @@
-import { authService } from 'myBase';
-import React from 'react';
+import { authService, dbService } from 'myBase';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
   };
+  const getMySuneets = async () => {
+    const suneets = await dbService.collection('suneets').where('creatorId', '==', userObj.uid).orderBy('createdAt').get();
+    console.log(suneets.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getMySuneets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
