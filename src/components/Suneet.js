e82af7e48d4d20906ad3497f2af3cea2d967e86c
SunEom <dkxl0701@gmail.com>
2020-12-30 14:06:14 +0900

    Update to edit Suneet

--- src/components/Suneet.js
+++ src/components/Suneet.js
@@ -1,7 +1,9 @@
 import { dbService } from 'myBase';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Suneet = ({ suneetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newSuneet, setNewSuneet] = useState(suneetObj.text);
   const onDeleteClick = async () => {
     const ok = window.confirm('Are you sure you want to delete this Suneet?');
     console.log(ok);
@@ -9,13 +11,39 @@ const Suneet = ({ suneetObj, isOwner }) => {
       await dbService.doc(`suneets/${suneetObj.id}`).delete();
     }
   };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.doc(`suneets/${suneetObj.id}`).update({
+      text: newSuneet,
+    });
+    setEditing(false);
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewSuneet(value);
+  };
   return (
     <div>
-      <h4>{suneetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button onClick={onDeleteClick}>Delete Suneet</button>
-          <button>Edit Suneet</button>
+          <form onSubmit={onSubmit}>
+            <input onChange={onChange} type="text" placeholder="Edit your Suneet" value={newSuneet} required />
+            <input type="submit" value="Update Suneet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{suneetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Suneet</button>
+              <button onClick={toggleEditing}>Edit Suneet</button>
+            </>
+          )}
         </>
       )}
     </div>
